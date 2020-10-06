6e6fe2f4d6b415ecc0f11181e59836d9f55da9cc
kentoda <kent19950811@gmail.com>
2020-10-06 08:02:29 +0000

    add  Tweet contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Tweet from "./Tweet";
 
 const App = () =>  {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+  <div>
+    <Tweet
+    icon="🍗"
+    displayName="筋肉太郎"
+    accountName="kinnikuman"
+    content="肉だけだったら、2キロ食べれます"
+  />
+  <Tweet
+    icon="🍜"
+    displayName="二郎系"
+    accountName="ramenma"
+    content="ぜんマシマシ"
+  />
+</div>
   );
 }
 

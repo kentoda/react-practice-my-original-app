edcc0b030a802ed7b0321707c478ad811e76a079
kentoda <kent19950811@gmail.com>
2020-10-06 08:00:30 +0000

    add file tweer.jsx

---
+++ src/Tweet.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Tweet = (props) => {
+   return (
+    <div className="tweet">
+      <div className="icon-container">{props.icon}</div>
+        <div className="body-container">
+          <div className="status-display">
+            <span className="display-name">{props.displayName}</span>
+            <span className="account-name">@{props.accountName}</span>
+          </div>
+        <div className="content">{props.content}</div>
+    </div>
+  </div>
+   )
+};
+
+export default Tweet;

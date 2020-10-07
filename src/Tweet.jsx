e2c691593fb7665160fafda8f721a1c13fea823f
kentoda <kent19950811@gmail.com>
2020-10-07 07:17:49 +0000

    Add  Like Function

--- src/Tweet.jsx
+++ src/Tweet.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
+
 
 const Tweet = (props) => {
+
+    // いいね機能useStateを使用しTweetコンポーネントに状態を持たせる
+   const [liked, setLike] = useState(false);
+
+   const toggleLike = useCallback(() => {
+        setLike((prev) => !prev)
+   },[setLike]);
+
+
    return (
     <div className="tweet">
       <div className="icon-container">{props.icon}</div>
@@ -10,6 +23,9 @@ const Tweet = (props) => {
             <span className="account-name">@{props.accountName}</span>
           </div>
         <div className="content">{props.content}</div>
+        <div className="status-action">
+        <span onClick={toggleLike}>{liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}</span>
+        </div>
     </div>
   </div>
    )
